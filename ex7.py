d07fe4b945ffb9b5b19b2f623b6f4622189d60b2
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-10-11 14:57:01 -0700

    Add files via upload
    As I said in an earlier submission Zed Shaw instructs us to have a friend break our code. From now on when I submit an exercise with "br.py" following the number in the title that means that it is a piece of clean code that I have sent to my mentor Michael so that he can break my code (thus "br" for "break"). I'll submit files titled "ex#brkn.py" with "brkn" standing for "broken" meaning that this is the broken code that Michael has sent me and I'll submit files titled "ex#fxd.py" with "fxd" standing for "fixed" so you can see my solutions to his problems.

---
+++ ex7.py
@@ -0,0 +1,38 @@
+# This line prints the string "Mary had a little lamb."
+print("Mary had a little lamb.")
+# This line prints the string embeds the string 'snow' into the larger string "Its fleece was white as {}" and then prints the new larger string.
+print("Its fleece was white as {}.".format('snow'))
+# This line prints the string "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+# this line prints and reprints the string "." ten times making one long string: ".........."
+print("." * 10)
+
+# This line creates the variable 'end1' and assignes it the value of the string "C".
+end1 = "C"
+# This line creats the variable 'end2' and assignes it the value of the string "h".
+end2 = "h"
+# This line creates the variable 'end3' and assignes it the value of the string "e".
+end3 = "e"
+# This line creates the variable 'end4' and assignes it the value of the string "e".
+end4 = "e"
+# This line creates the variable 'end5' and assinges it the value of the string "s".
+end5 = "s"
+# This line creates the varable 'end6' and assinges it the value of the string "e".
+end6 = "e"
+# This line creates the variable 'end7' and assinges it the value of the string "B".
+end7 = "B"
+# This line creates the variable 'end8' and assinges it the value of the string "u".
+end8 = "u"
+# This line creates the variable 'end9' and assinges it the value of the string "r".
+end9 = "r"
+# This line create the variable 'end10' and assignes it the value of the string "g".
+end10 = "g"
+# This line creates the variable 'end11' and assinges it the value of the string "e".
+end11 = "e"
+# This line creates the variable 'end12' and assignes it the value of the string "r".
+end12 = "r"
+
+# This line combines the values of the variables 'end1', 'end2', 'end3', 'end4', 'end5', 'end6', and 'end' and prints them into the string "Cheese ".
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# This line combines the values of the variables 'end7', 'end8', 'end9', 'end10', 'end11', and 'end12' and prints them into the string "Burger".
+print(end7 + end8 + end9 + end10 + end11 + end12)
