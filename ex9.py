29e28371d82cad9481adf2de19b12d286ef148bb
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-10-13 08:52:09 -0700

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# This line creats the var 'days' and assigns it the value of the string "Mon Tue Wed Thu Fri Sat Sun"
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# This line creates the var 'months' and give it the value of the string "Jun\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" and prints that string.The '\n' symbol causes the peices of the string that follow it to appear on a new line.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# This line prints the string "Here are the days: " and then prints the value of the var 'days'.
+print("Here are the days: ", days)
+# This line prints the string "Here are the months: " and the value of var 'months'.
+print("Here are the months: ", months)
+
+# This line prints the string "There's something goin on here. With the three double-quotes. We'll be able to type as much as we like. Even 4 lines if we want, or 5, or 6." However, The string is enclosed by three quote symbols wich causes it to be printed in exactly the manner in which it is formated: spread out over four lines.
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
