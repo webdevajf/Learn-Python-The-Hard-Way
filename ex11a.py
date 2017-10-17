9e4e7c037d85f6255d522b958dd6d145f7b9bad4
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-10-17 13:06:38 -0700

    Add files via upload
    Learning about input's and how they work. It's the first opportunity I've had to to really fiddle with something, which is complicated enough, that there is a more efficient and a less efficient way of doing it.

---
+++ ex11a.py
@@ -0,0 +1,15 @@
+print("Do you like coffee or tea?", end= '')
+poison = input()
+print("Do you take it with milk or without milk?", end= '')
+cloudy_or_no = input()
+print("Do you take it with sugar or unsweetened?", end= '')
+sweettooth = input()
+
+print(f"So you like to drink {poison}, {cloudy_or_no}, and you take it {sweettooth}.")
+
+ print('-----------------')
+
+ applicant = input("Enter applicant's name: ")
+ interviewer = input("Enter the interviewer's name: ")
+ time = input ("Enter the appointment time: ")
+ print (interviewer, "will interview", applicant, "at", time, ".")
---
+++ ex11b.py
@@ -0,0 +1,16 @@
+# print("Do you like coffee or tea?", end= '')
+# poison = input()
+# print("Do you take it with milk or without milk?", end= '')
+# cloudy_or_no = input()
+# print("Do you take it with sugar or unsweetened?", end= '')
+# sweettooth = input()
+
+# print(f"So you like to drink {poison}, {cloudy_or_no}, and you take it {sweettooth}.")
+
+# Try to find another, more efficient, way to write the program above.
+
+poison = input("Do you like coffee or tea? ")
+cloudy_or_no = input("Do you take it with milk or without milk? ")
+sweettooth = input("Do you take it with sugar or unsweetened? ")
+
+print("So you like", poison, ",", cloudy_or_no, ", and you take it", sweettooth, ".")
