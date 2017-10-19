2ffc90808f344b9a355c6df21cc37ee97b703afc
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-10-19 09:56:19 -0700

    Add files via upload
    Combining 'import' and 'input()' together in the same file. I haven't yet succeed in combining the two in passing a single argument. I want to figure out how to do that.

---
+++ ex13a.py
@@ -0,0 +1,10 @@
+temperature = input("What changes the state of H2O?")
+
+from sys import argv
+script, one, two, three = argv
+
+print("It starts as", one + ".")
+print("Add some", temperature + ":")
+print("it becomes", two + ".")
+print("Add a little more", temperature + ":")
+print("it becomes", three + "!")
