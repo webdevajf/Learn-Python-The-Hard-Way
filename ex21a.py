aeb545f89151ab0d30afcf157e094b28289d1720
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-11-09 10:28:16 -0800

    Add files via upload
    patch for ex21a.py 
    I further fleshed out the ideas therein.

--- ex21a.py
+++ ex21a.py
@@ -1,13 +1,15 @@
+# These lines are an attempt to work out how the
+# 'return' function works.
 def foo (a, b):
-    print(f"foo is: {a} + {b}")
+    print(f"foo is {a} + {b}")
     return a + b
 
-#def bar():
-#    print("bar is foo * 2")
-#    return foo * 2
+def bar(a):
+    print("bar is foo * 2")
+    return a * 2
 
 f = foo(1, 2)
+print("foo = ", f)
 
-print(f"foo is: {f}")
-
-#bar()
+b = bar(f)
+print("bar = ", b)
