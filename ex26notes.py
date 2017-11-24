14ffd8f07732774587192fdfe5955d54117f4dae
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-11-24 13:15:51 -0800

    Add files via upload
    A quiz in which we are supposed to take broken, imperfect, or poorly written code, from another programer, and fix it and clean it up so that it runs better. I'll be submitting my work on this in installments (via branches that I'll integrate into the master branch) as I progress through it.

---
+++ ex26notes.py
@@ -0,0 +1,142 @@
+# This line is intended to print a string that
+# querreys the user for an input.
+# PROBLEMS W/ THIS LINE: None. However, if I
+# were to write this I'd have combined the var
+# and the print statement by making the string
+# into the querry within the print statement.
+print("How old are you?", end=' ')
+# This line is intended to create the var 'age'
+# and ask the user for an input to defign the
+# var.
+# PROBLEMS W/ THIS LINE: There's could be a prompt
+#('>' for example) to inform the user that an input
+# is needed. However, the question in the print
+# statement probably does this adequatley.
+age = input()
+# This line is intended to print a string that
+# querreys the user for an input.
+# PROBLEMS W/ THIS LINE: There's no variable,
+# with an 'input()' assigned to it's value, following
+# this printed querry. Furthermore, if I
+# were to write this I'd have made the string in
+# this print statement the querry for an input
+# in a var 'Height'.
+print("How tall are you?", end=' ')
+# This line is intended to querry the user for
+# an input() to defign a var in the next line.
+# PROBLEMS W/ THIS LINE: There's no end braket
+# (')') to close the print statemnt.
+print("How much do you weigh?", end=' '
+# This line is intended to create a var 'weight'
+# and querry the user for an input to defign the
+# value of the var.
+# PROBLEMS W/ THIS LINE: none.
+weight = input()
+
+# This line is intended to print a formated string
+# with the values of the var's 'age', 'height', and
+# 'weight' inserted, respectively, into the
+# placeholder brackets within.
+# PROBLEMS W/ THIS LINE: I would have followed the
+# placeholder {age} with the string "years old,"
+# instead of just "old," because when typing the
+# answer to the input querry "How old are you?"
+# I think the typical english speaker is more likely
+# to simply type the number of their age rather than
+# the number of their age + the word 'years'.
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+script, filename = argv
+
+txt = open(filenme)
+
+print("Here's your file {filename}:")
+print(tx.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again_read())
+
+
+print('Let's practice everything.')
+print('You\'d need to know \'bout escapes
+      with \\ that do \n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("--------------)
+print(poem)
+print(--------------")
+
+
+five = 10 - 2 + 3 -
+print(f"This should be five: {five}"
+
+def secret_formula(started)
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars  100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+formula = secret_formula(startpoint)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+
+
+
+people = 20
+cates = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+
+if people < cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs
+    print("People are less than or equal to dogs.)
+
+
+if people = dogs:
+    print("People are dogs.")
+
+
+
+# This line is intended to
+# PROBLEMS W/ THIS LINE:
