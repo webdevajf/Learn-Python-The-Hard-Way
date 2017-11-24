20cd11cab9e3144a69b85d8a529206936aca5648
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-11-24 13:52:58 -0800

    Add files via upload

--- ex26test.py
+++ ex26test.py
@@ -1,5 +1,13 @@
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh? ")
+#age = input("How old are you? ")
+#height = input("How tall are you? ")
+#weight = input("How much do you weigh? ")
 
-print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+#print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+from sys import argv
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
