8bb2d010369a9589545cf263e5dc161eb7439b0d
webdevajf <32581698+webdevajf@users.noreply.github.com>
2017-12-16 21:10:59 -0500

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platupus']
+
+print("#1 The animal at 1 is the 2nd animal and is a 'python3.6'.", animals[1])
+print("#2 The third (3rd) animal is at 2 and is a 'peacock'.", animals[2])
+print("#3 The first (1st) animal is at 0 and is a 'bear'.", animals[0])
+print("#4 The animal at 3 is the 4th animal and is a 'kangaroo'.", animals[3])
+print("#5 The fifth (5th) animal is at 4 and is a 'whale'.", animals[4])
+print("#6 The animal at 2 is the 3rd animal and is a 'peacock'.", animals[2])
+print("#7 The sixth (6th) animal is at 5 and is a 'platypus'.", animals[5])
+print("#8 The animal at 4 is the fifth animal is a 'whale'.", animals[4])
---
+++ ex34a.py
@@ -0,0 +1,6 @@
+beatles = ['George', 'John', 'Paul', 'Ringo']
+
+print('George is at 0 and is the first beatle in the list.', beatles[0])
+print('3 contains Ringo who is the 4th beatle on the list.', beatles[3])
+print('Paul is the 3rd beatle which puts him at 2.', beatles[2])
+print('John is the 2nd beatle on the list because he is at 1.', beatles[1])
