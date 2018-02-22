26552e60daaaf993dde850841adf7046c71a5878
webdevajf <32581698+webdevajf@users.noreply.github.com>
2018-02-22 15:44:39 -0800

    Add files via upload

---
+++ ex37.py
@@ -0,0 +1,303 @@
+### KEYWORDS ###
+
+and
+# bullion logical 'and'. adds two parameters together
+# to create a True/ False statement according to
+# bullion logic.
+
+as
+# ?
+
+assert
+# ?
+
+break
+# ?
+
+class
+# ?
+
+continue
+# ?
+
+def
+# creats a function.
+
+del
+# ?
+
+elif
+# creates a branch in a function when there are three
+# or more branches.
+
+else
+# creats the final potential branch in a function
+# containing a branching program of any number of
+# possibilites that is >= 2.
+
+except
+# ?
+
+exec
+# ?
+
+finally
+# ?
+
+for
+# creates a 'for' loop which passes a value or set of
+# values into the function contained within and then
+# returns a resulting value or set of values made
+# by having the function act upon the passed through
+# values. A 'for' loop can also be made to pass its
+# returned value or values into a list var.
+
+from
+# ?
+
+global
+# ?
+
+if
+# creates a branching program and provides for the
+# first potential branch in that program.
+
+import
+# imports a module into this one to use.
+
+in
+# part of a for loop which connects the list to be
+# passed into the function's arg ('the_count') and
+# establishes the arg into which the value(s) of
+# that list is(are) to be passed into ('number')
+
+# ex:
+# the_count = [1, 2, 3]
+#
+# for number in the_count:
+#   print(f"This is the count {number}")
+#
+# Will return:
+# This is the count 1
+# This is the count 2
+# This is the count 3
+
+is
+# ?
+#?
+
+
+# lambda
+# ?
+
+not
+# logical not. In bullion logic = adds a 'False +'
+# to whatever statment follows it.
+
+or
+# logical or. I understand this concept implicitly
+# but don't know how to put it into words...
+# LOOK THIS UP.
+
+pass
+# ?
+
+print
+# prints a value contained within it's parenthesese
+
+raise
+# ?
+
+return
+# ?
+# we've studied this but I've become sorely aware of
+# how poorly I understand it. NEED TO review.
+
+try:
+# ?
+
+while
+# creates a 'while' loop which continues to run its
+# code indefinitely until it's condition for running
+# is no longer being met.
+
+with
+# ?
+
+yield
+# ?
+
+
+### DATA TYPES ###
+
+True
+# True boolean value.
+
+False
+# False boolean value.
+
+None
+# ?
+
+bytes
+# ?
+
+strings
+# Stores textual information
+
+numbers or integers
+# Stores integers
+
+floats
+# stores decimals
+
+lists
+# stores a list of things
+
+dicts
+# ?
+
+
+### STRING ESCAPE SEQUENCES ###
+
+\\
+# (escaped) Backslash
+
+\'
+# (escaped) Single-quote
+
+\"
+# (escaped) Double-quote
+
+\a
+# ?
+
+\b
+# Backspace
+
+\f
+# ?
+
+\n
+# Newline
+
+\r
+# ?
+
+\t
+# Tab
+
+\v
+# Vertical Tab
+
+
+### OLD STYLE STRING FORMATS ###
+
+%d
+# ?
+
+%i
+# ?
+
+%o
+# ?
+
+%u
+# ?
+
+%x
+# ?
+
+%X
+# ?
+
+%e
+# ?
+
+%E
+# ?
+
+%f
+# ?
+
+%F
+# ?
+
+%g
+# ?
+
+%G
+# ?
+
+%c
+# ?
+
+%r
+# ?
+
+%s
+# ?
+
+%%
+# ?
+
+
+### OPERATORS ###
+
++
+
+-
+
+*
+
+**
+
+/
+
+//
+
+%
+
+<
+
+>
+
+<=
+
+>=
+
+==
+
+!=
+
+()
+
+[]
+
+{}
+
+@
+
+,
+
+:
+
+.
+
+=
+
+;
+
++=
+
+-=
+
+*=
+
+/=
+
+//=
+
+%=
+
+**=
